2a109a0e5cc0410b53cf00759ddabc7dabd0aaf3
oaksk <oaksk6820@gmail.com>
2026-06-09 17:45:34 +0700

    remove reverb in start.sh

--- api/start.sh
+++ api/start.sh
@@ -1,5 +1,5 @@
+#!/bin/sh
 php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
-php artisan reverb:start --host=0.0.0.0 --port=8080 &
 php artisan serve --host=0.0.0.0 --port=$PORT
